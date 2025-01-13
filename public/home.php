48eec92cc6fe2590d3613f639a46394a46f691a3
gaksss <clement.carlier2@gmail.com>
2025-01-13 20:48:26 +0100

    Correction Answers

--- public/home.php
+++ public/home.php
@@ -6,11 +6,13 @@ include_once("../utils/autoloader.php");
 $qcm = new Qcm("Johnny Halliday");
 
 $question = new Question("Johnny ou Madison?");
-$answers =  new Answer(true, "Madison");
+$answers =  [new Answer(true, "Madison"),
+new Answer(false, "Johnny"),
+new Answer(false, "Ton père")];
 
 
 $qcm->addQuestion($question);
-$question->addAnswer($answer);
+$question->setAnswers($answers);
 
 
 var_dump($qcm);
